4c221dddfb7519b5904857023158663da468fbdb
Silvina Ciampechini <silciampe@gmail.com>
2023-05-12 16:24:59 -0300

    todo resuelto

---
+++ home.js
@@ -0,0 +1,34 @@
+import { getCharacters } from "/src/services/getData.js";
+
+const container = document.querySelector('#characters');
+const loader = document.getElementById('lds-ring');
+
+const charactersList = async (page = 1) => {
+    loader.style.display = 'grid';
+    const {results} = await getCharacters(page);
+
+    loader.style.display = 'none';
+    results.forEach(character => {
+        const article = document.createElement('article');
+        article.setAttribute('class', 'character');
+        article.innerHTML = `
+            <img src="${character.image}" alt="">
+            <h2>${character.name}</h2>
+            <div>
+                <p>${character.species}</p>
+                <p class="${character.status.toLowerCase()}"></p>
+            </div>
+            <a href="/#/${character.id}">Ver detalle
+            </a>
+        `; 
+        container.appendChild(article);
+    })
+}
+
+window.addEventListener('hashchange', () => {
+    const id = location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/';
+    localStorage.setItem('charID',id);
+    window.location.replace('/character.html');
+});
+
+charactersList();
